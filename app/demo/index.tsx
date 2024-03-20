9b14a63179e033012b85eb31f1042f75ceff2078
Devmax <maxsamuel214@protonmail.com>
2024-03-20 06:26:27 -0700

    Update routes

--- app/demo/index.tsx
+++ app/demo/index.tsx
@@ -1,9 +1,16 @@
-import { Outlet } from '@remix-run/react';
+import { useLoaderData, Outlet } from '@remix-run/react';
+import { json } from "@remix-run/node";
+
+export const loader = async () => {
+  return json({ title: 'Demo' });
+};
 
 export default function Demo() {
+  const { title } = useLoaderData<any>();
+
   return (
     <div>
-      <h3>Demo Routing</h3>
+      <h3>{title} Routing</h3>
       <Outlet />
     </div>
   )
